1bda6d03510bd81c0d7fc59fdd084ce6040c3f92
Kin (AKA: Suel) <suelgarabagli2@gmail.com>
2026-01-31 21:12:51 +0400

    код крутой я закончил пытки

--- library/src/main/java/com/library/dea/controller/BookPageController.java
+++ library/src/main/java/com/library/dea/controller/BookPageController.java
@@ -3,6 +3,7 @@ package com.library.dea.controller;
 import com.library.dea.entity.Book;
 import com.library.dea.service.BookService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -20,8 +21,13 @@ public class BookPageController {
 
     // table
     @GetMapping // mvc
-    public String showBooks(Model model) {
+    public String showBooks(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "5") int size,Model model) {
+        Page<Book> bookPage = bookService.findPaginated(page, size);
         model.addAttribute("books", bookService.showAll());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", bookPage.getTotalPages());
+        model.addAttribute("totalItems", bookPage.getTotalElements());
+        model.addAttribute("size", size);
         return "library/list";
     }
 
@@ -52,6 +58,22 @@ public class BookPageController {
     public String delete(@PathVariable Integer id){
         bookService.deleteBook(id);
         return "redirect:/books";
+    }
+
+    @GetMapping("/search")
+    public String searchBooks(@RequestParam String keyword,
+                              @RequestParam(defaultValue = "0") int page,
+                              @RequestParam(defaultValue = "5") int size,
+                              Model model) {
+        Page<Book> bookPage = bookService.search(keyword, page, size);
+
+        model.addAttribute("books", bookPage.getContent());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", bookPage.getTotalPages()); 
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("size", size);
+
+        return "library/list";
     }
 
 }
